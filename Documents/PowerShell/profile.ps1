7c827e34a2ba51205b8bca7a1ac32a0db48825e7
Paya Maroufi <h2o.Drop2010@gmail.com>
2026-06-23 06:30:47 -0700

    feat: Add powershell profile

---
+++ Documents/PowerShell/profile.ps1
@@ -0,0 +1,14 @@
+oh-my-posh.exe init pwsh --config "~/.config.omp.toml" | Invoke-Expression
+
+
+Import-Module ZLocation
+
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
+
+$Env:PYTHONSTARTUP = "~\.pythonrc"
+
+fastfetch
